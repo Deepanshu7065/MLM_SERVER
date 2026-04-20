25ad9f95cbc4188d975a75ccc60fad1d82039814
Deepanshu7065 <step.deepanshu10@gmail.com>
2026-04-20 13:16:25 +0530

    dddd

--- src/DB/sequelize.js
+++ src/DB/sequelize.js
@@ -1,19 +1,17 @@
 import { Sequelize } from "sequelize";
 
-// Sirf DATABASE_URL ka istemal karein
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
-  // Production/Supabase ke liye SSL zaroori hai
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false 
     }
   },
   pool: {
     max: 10,
-    min: 2,
+    min: 0,
     acquire: 30000,
     idle: 10000,
   },
