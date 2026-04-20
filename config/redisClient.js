4cb0b399641d5912ff7dad8c6c9676fe3ab5a723
Deepanshu7065 <step.deepanshu10@gmail.com>
2026-04-20 13:10:06 +0530

    dddd

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,17 +1,33 @@
+// import Redis from "ioredis";
+
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST || "redis",
+//   port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+//   password: process.env.REDIS_PASSWORD || undefined,
+//   // optional: set lazyConnect: true
+// });
+
+// redis.on("error", (err) => {
+//   console.error("Redis error:", err);
+// });
+// redis.on("connect", () => {
+//   console.log("Connected to Redis");
+// });
+
+// export default redis;
+
+
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "redis",
-  port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-  // optional: set lazyConnect: true
-});
+// Direct connection string use karein jo dashboard par set hai
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("error", (err) => {
   console.error("Redis error:", err);
 });
+
 redis.on("connect", () => {
-  console.log("Connected to Redis");
+  console.log("Connected to Redis ✅");
 });
 
 export default redis;
