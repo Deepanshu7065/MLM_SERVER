81516466af50898a34ba0b8a16608b0f92597b88
Deepanshu7065 <step.deepanshu10@gmail.com>
2026-04-20 12:45:03 +0530

    another data base connect change

--- src/DB/index.js
+++ src/DB/index.js
@@ -2,14 +2,26 @@
 
 //  /src/DB/index.js
 
+// import pg from "pg";
+
+// const pool = new pg.Pool({
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+// });
+
+// export default pool;
+
+
 import pg from "pg";
 
 const pool = new pg.Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
