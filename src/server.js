c0cc1c90d8c809a5ffc01b82945c9a428da4ac7b
Deepanshu7065 <step.deepanshu10@gmail.com>
2026-04-30 15:56:11 +0530

    fix url

--- src/server.js
+++ src/server.js
@@ -387,7 +387,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://dm-advancetech.com",
     methods: ["GET", "POST"]
   }
 });
